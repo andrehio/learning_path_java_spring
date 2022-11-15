d2136766d8e7081be91245c880355b1102cb626d
andrehio <ahdreki@gmail.com>
2022-11-15 09:48:27 -0300

    .

---
+++ ex_0003_inversaoDeControle/src/inversaoDeControle/Pedido.java
@@ -0,0 +1,17 @@
+package inversaoDeControle;
+
+public class Pedido {
+	
+	private EnviarEmails enviar = EnviarEmails.obterDadosEmail();
+
+	public static void main(String[] args) {
+		Pedido pedido = new Pedido();
+		
+		pedido.gravar();
+	}
+	
+	public void gravar() {
+		this.enviar.retornar("Pedido criado!");
+	}
+
+}
