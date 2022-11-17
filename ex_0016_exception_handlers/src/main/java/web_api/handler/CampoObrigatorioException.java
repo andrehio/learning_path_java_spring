232b1700101fdfe035a5531a2ebcf3dc118ee219
andrehio <ahdreki@gmail.com>
2022-11-17 08:00:37 -0300

    .

---
+++ ex_0016_exception_handlers/src/main/java/web_api/handler/CampoObrigatorioException.java
@@ -0,0 +1,9 @@
+package web_api.handler;
+
+public class CampoObrigatorioException extends BusinessException{
+
+	public CampoObrigatorioException(String campo) {
+		super("O campo %s é obrigatório", campo);
+	}
+	
+}
